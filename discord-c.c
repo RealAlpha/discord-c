72e8a46e1eb6637542dff926534b5934e5d6a613
Luc Thomas <alphaverta@gmail.com>
2017-07-13 13:24:55 +0000

    Add returning of messages to getMessagesInChannel

--- discord-c.c
+++ discord-c.c
@@ -10,7 +10,6 @@
 #include <curl/curl.h>
 
 void sendMessage(/* TODO some kind of connection object?, */char *content, uint64_t channel, uint8_t isTTS);
-void getMessagesInChannel(uint64_t channel, int ammount); // TODO create before/around/after functions too?
 
 int client_ws_receive_callback(client_websocket_t *socket, char *data, size_t length);
 int client_ws_connection_error_callback(client_websocket_t* socket, char* reason, size_t length);
@@ -161,6 +160,8 @@ void freeChannels(struct server_channel *node);
 void freeUsers(struct server_user *node);
 void freeUserRoles(struct roles *node);
 void freeRoles(struct roles *node);
+
+struct messages *getMessagesInChannel(uint64_t channel, int ammount); // TODO create before/around/after functions too?
 
 // TODO remove - for memory leak stuff test yeah...
 client_websocket_t *globWebSocket = NULL;
@@ -1032,9 +1033,7 @@ void init_string(struct string *s) {
   s->ptr[0] = '\0';
 }
 
-void processMessages(char *json);
-
-void getMessagesInChannel(uint64_t channel, int amount)
+struct messages *getMessagesInChannel(uint64_t channel, int amount)
 {
 	if (amount > 100 || amount < 0)
 	{
@@ -1044,7 +1043,8 @@ void getMessagesInChannel(uint64_t channel, int amount)
 
 	curl_global_init(CURL_GLOBAL_DEFAULT);
 	CURL *curl = curl_easy_init();
-	
+	struct messages *messages = NULL;
+
 	if (curl) {
 		// TODO make this buffer the correct size
 		char requestUri[80];
@@ -1071,116 +1071,109 @@ void getMessagesInChannel(uint64_t channel, int amount)
 		CURLcode res;
 		res = curl_easy_perform(curl);
 		
-		printf("\n\nResult:\n%s\n\n", s.ptr);
+		cJSON *root = cJSON_Parse(s.ptr);
 		
-		processMessages(s.ptr);
+		if (!root->child)
+			return NULL;
 
+		cJSON *channelIdObject = cJSON_GetObjectItemCaseSensitive(root->child, "channel_id");
+
+		uint64_t channelId = strtoull((const char *)channelIdObject->valuestring, NULL, 10);
+
+
+		// Attempt to find the correct server/channel - as this is a get messages in channel call it shouldn't change!
+		struct server *server = glob_servers;
+		struct server_channel *channel = NULL;
+
+		while(server)
+		{
+			// Did it already find the channel? (AKA not eqaul to NULL)
+			if (channel)
+				break;
+
+			printf("Server id: %lu|Server name: %s\n", server->serverId, server->name);
+			struct server_channel *_channel = server->channels;
+			while (_channel)
+			{
+				//printf("Channel id: %llu|Required id: %llu|Channel Name: %s", _channel->id, channelId, _channel->name);
+				if (channelId == _channel->id)
+				{
+					printf("Found channel with name: %s\n", _channel->name);
+					channel = _channel;
+					break;
+				}
+
+				_channel = _channel->next;
+			}
+
+			// Here again so it doesn't switch to next...helps preserve the server variable
+			if (channel)
+				break;
+
+			server = server->next;
+		}
+
+		if(server == NULL || channel == NULL)
+		{
+			printf("Error while recieving message! User not in channel!\n");
+			return NULL;
+		}
+
+
+		// Itterate over the messages;
+		cJSON *message = root->child;
+
+		while (message)
+		{
+			cJSON *authorObject = cJSON_GetObjectItemCaseSensitive(message, "author");
+			cJSON *contentObject = cJSON_GetObjectItemCaseSensitive(message, "content");
+			// TODO add webhook handling + DM handling
+			cJSON *userIdObject = cJSON_GetObjectItemCaseSensitive(authorObject, "id");
+		
+			uint64_t userId = strtoull((const char *)userIdObject->valuestring, NULL, 10);
+
+			// Try to find the user.
+			struct server_user *user = server->users;
+			while (user)
+			{
+				//printf("Currently itterating over user: %s (%llu). Required: %llu\n", user->user->username, user->user->id, userId);
+				if (user->user->id == userId)
+				{
+					// Found user! (no need to copy it as breaking here will keep user where it currently is)
+					break;
+				}
+
+				user = user->next;
+			}
+
+			if (user == NULL)
+			{
+				printf("Unable to find author of the message!\n");
+				return NULL;
+			}
+
+
+			// Stuff it all in a struct
+			struct message _message;
+			_message.author = user; // TODO should be user->user instead?
+			_message.channel = channel;
+			_message.server = server;
+			_message.body = contentObject->valuestring; // TODO strcopy it instead (current solution gets freed once this function retruns)? If so, make a linked list of messages (message-chain) so it can easily be freed.
+
+			struct messages *__message = malloc(sizeof(struct messages));
+
+			__message->message = _message;
+
+			__message->next = messages;
+			messages = __message;
+
+			message = message->next;
+		}
 		// Cleanup time!
 		curl_easy_cleanup(curl);
+		// TODO use strcpy + a message chain to clean up the messages
 	}
 	curl_global_cleanup();
-}
 
-void processMessages(char *json)
-{
-	cJSON *root = cJSON_Parse(json);
-	
-	if (!root->child)
-		return;
-
-	cJSON *channelIdObject = cJSON_GetObjectItemCaseSensitive(root->child, "channel_id");
-
-	uint64_t channelId = strtoull((const char *)channelIdObject->valuestring, NULL, 10);
-
-	
-	// Attempt to find the correct server/channel - as this is a get messages in channel call it shouldn't change!
-	struct server *server = glob_servers;
-	struct server_channel *channel = NULL;
-	
-	while(server)
-	{
-		// Did it already find the channel? (AKA not eqaul to NULL)
-		if (channel)
-			break;
-
-		printf("Server id: %lu|Server name: %s\n", server->serverId, server->name);
-		struct server_channel *_channel = server->channels;
-		while (_channel)
-		{
-			//printf("Channel id: %llu|Required id: %llu|Channel Name: %s", _channel->id, channelId, _channel->name);
-			if (channelId == _channel->id)
-			{
-				printf("Found channel with name: %s\n", _channel->name);
-				channel = _channel;
-				break;
-			}
-			
-			_channel = _channel->next;
-		}	
-		
-		// Here again so it doesn't switch to next...helps preserve the server variable
-		if (channel)
-			break;
-
-		server = server->next;
-	}
-
-	if(server == NULL || channel == NULL)
-	{
-		printf("Error while recieving message! User not in channel!\n");
-		return;
-	}
-
-	
-	// Itterate over the messages;
-	cJSON *message = root->child;
-	struct messages *messages = NULL;
-
-	while (message)
-	{
-		cJSON *authorObject = cJSON_GetObjectItemCaseSensitive(message, "author");
-		cJSON *contentObject = cJSON_GetObjectItemCaseSensitive(message, "content");
-		// TODO add webhook handling + DM handling
-		cJSON *userIdObject = cJSON_GetObjectItemCaseSensitive(authorObject, "id");
-		
-		uint64_t userId = strtoull((const char *)userIdObject->valuestring, NULL, 10);
-
-		// Try to find the user.
-		struct server_user *user = server->users;
-		while (user)
-		{
-			//printf("Currently itterating over user: %s (%llu). Required: %llu\n", user->user->username, user->user->id, userId);
-			if (user->user->id == userId)
-			{
-				// Found user! (no need to copy it as breaking here will keep user where it currently is)
-				break;
-			}
-
-			user = user->next;
-		}
-	
-		if (user == NULL)
-		{
-			printf("Unable to find author of the message!\n");
-			return;
-		}
-		
-
-		// Stuff it all in a struct
-		struct message _message;
-		_message.author = user; // TODO should be user->user instead?
-		_message.channel = channel;
-		_message.server = server;
-		_message.body = contentObject->valuestring; // TODO strcopy it instead (current solution gets freed once this function retruns)? If so, make a linked list of messages (message-chain) so it can easily be freed.
-		
-		struct messages *__message = malloc(sizeof(struct messages));
-		
-		__message->message = _message;
-
-		__message->next = messages;
-		messages = __message;
-
-		printf("Message:\n%s\n(by: %s (%lu) in %s/%s)\n", _message.body, _message.author->user->username, _message.author->user->id, _message.server->name, _message.channel->name);
-		message = message->next;
-	}
+	return messages;
 }
